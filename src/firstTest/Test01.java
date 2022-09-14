c6ced780c54a697c14269772d5d351b5bee3dd84
Lakshminathac <lakshminatha.chakka@gmail.com>
2022-09-14 11:10:25 +0530

    First Commit

---
+++ src/firstTest/Test01.java
@@ -0,0 +1,12 @@
+package firstTest;
+
+public class Test01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		
+		System.out.println("this is first test with GIT");
+
+	}
+
+}
